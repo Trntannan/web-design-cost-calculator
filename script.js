d6e80b455ae6ef300c3bd3e25b40b74ae10e37ad
trent <trntannan1@gmail.com>
2024-05-22 03:15:32 +1200

    added 'platform' cost adjustments

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   // Function to handle input changes for page count and SEO count
   function handleInputChanges(inputId, valueId) {
-      let input = document.getElementById(inputId);
-      let value = document.getElementById(valueId);
-      input.addEventListener("input", function() {
-          value.textContent = this.value;
-      });
+    let input = document.getElementById(inputId);
+    let value = document.getElementById(valueId);
+    input.addEventListener("input", function () {
+      value.textContent = this.value;
+    });
   }
 
   // Call handleInputChanges function for page count and SEO count
@@ -14,16 +14,20 @@ document.addEventListener("DOMContentLoaded", function() {
 
   // Function to handle button clicks for styling, e-com, CMS, and database
   function handleButtonClicks(groupClassName, valueClassName) {
-      document.querySelectorAll("." + groupClassName + " .choice-btn").forEach((button) => {
-          button.addEventListener("click", () => {
-              document.querySelectorAll("." + groupClassName + " .choice-btn").forEach((btn) => {
-                  btn.classList.remove("chosen");
-                  btn.classList.add("not-chosen");
-              });
-              button.classList.remove("not-chosen");
-              button.classList.add("chosen");
-              console.log(button.value);
-          });
+    document
+      .querySelectorAll("." + groupClassName + " .choice-btn")
+      .forEach((button) => {
+        button.addEventListener("click", () => {
+          document
+            .querySelectorAll("." + groupClassName + " .choice-btn")
+            .forEach((btn) => {
+              btn.classList.remove("chosen");
+              btn.classList.add("not-chosen");
+            });
+          button.classList.remove("not-chosen");
+          button.classList.add("chosen");
+          console.log(button.value);
+        });
       });
   }
 
@@ -35,24 +39,24 @@ document.addEventListener("DOMContentLoaded", function() {
 
   // Handle SEO button clicks
   document.querySelectorAll(".seo-btn").forEach((button) => {
-      button.addEventListener("click", () => {
-          document.querySelectorAll(".seo-btn").forEach((btn) => {
-              btn.classList.remove("chosen");
-              btn.classList.add("not-chosen");
-          });
-          button.classList.remove("not-chosen");
-          button.classList.add("chosen");
-
-          if (button.value === "On-page SEO") {
-              console.log("On-page SEO");
-              document.getElementById("seo-page-count").style.display = "inline";
-              document.getElementById("seo-page-count-value").style.display = "inline";
-          } else if (button.value === "Technical SEO") {
-              console.log("Technical SEO");
-              document.getElementById("seo-page-count").style.display = "none";
-              document.getElementById("seo-page-count-value").style.display = "none";
-          }
+    button.addEventListener("click", () => {
+      document.querySelectorAll(".seo-btn").forEach((btn) => {
+        btn.classList.remove("chosen");
+        btn.classList.add("not-chosen");
       });
+      button.classList.remove("not-chosen");
+      button.classList.add("chosen");
+
+      if (button.value === "On-page SEO") {
+        console.log("On-page SEO");
+        document.getElementById("seo-page-count").style.display = "inline";
+        document.getElementById("seo-page-count-value").style.display ="inline";
+      } else if (button.value === "Technical SEO") {
+        console.log("Technical SEO");
+        document.getElementById("seo-page-count").style.display = "none";
+        document.getElementById("seo-page-count-value").style.display = "none";
+      }
+    });
   });
 });
 
@@ -73,64 +77,82 @@ function calculatePrice() {
   let stylePrice = 0;
   let styleType = document.querySelector(".style .choice-btn.chosen").value;
   if (styleType === "Simple") {
-      stylePrice = 1000;
+    stylePrice = 1000;
   } else if (styleType === "Moderate") {
-      stylePrice = 3000;
+    stylePrice = 3000;
   } else if (styleType === "High-End") {
-      stylePrice = 5000;
+    stylePrice = 5000;
   } else if (styleType === "World Class") {
-      stylePrice = 10000;
+    stylePrice = 10000;
   }
 
   let ecommercePrice = 0;
   let ecommerceType = document.querySelector(".e_com .choice-btn.chosen").value;
   if (ecommerceType === "Basic") {
-      ecommercePrice = 3000;
+    ecommercePrice = 3000;
   } else if (ecommerceType === "Advanced") {
-      ecommercePrice = 6000;
+    ecommercePrice = 6000;
   } else if (ecommerceType === "Enterprise") {
-      ecommercePrice = 10000;
+    ecommercePrice = 10000;
   }
 
   let databasePrice = 0;
-  let databaseType = document.querySelector(".data_base .choice-btn.chosen").value;
+  let databaseType = document.querySelector(
+    ".data_base .choice-btn.chosen"
+  ).value;
   if (databaseType === "Basic") {
-      databasePrice = 2000;
+    databasePrice = 2000;
   } else if (databaseType === "Advanced") {
-      databasePrice = 5000;
+    databasePrice = 5000;
   } else if (databaseType === "Full") {
-      databasePrice = 10000;
+    databasePrice = 10000;
   }
 
   let cmsPrice = 0;
   let cmsType = document.querySelector(".cms .choice-btn.chosen").value;
   if (cmsType === "Basic") {
-      cmsPrice = 2000;
+    cmsPrice = 2000;
   } else if (cmsType === "Advanced") {
-      cmsPrice = 4000;
+    cmsPrice = 4000;
   } else if (cmsType === "Enterprise") {
-      cmsPrice = 8000;
+    cmsPrice = 8000;
   }
 
-  let platformPrice = parseInt(document.querySelector(".platformOptions").value);
+  let platformPrice = 0;
+  // Applying platform cost adjustments
+  if (platformType === "Custom") {
+    platformPrice = 0;
+  } else if (platformType === "Webflow") {
+    platformPrice = 0.9; // 10% discount
+  } else if (platformType === "Shopify" || platformType === "Squarespace" || platformType === "Wordpress" || platformType === "Woocommerce") {
+    platformPrice = 0.75; // 25% discount
+  } else if (platformType === "Wix" || platformType === "Rocketspark") {
+    platformPrice = 0.7; // 30% discount
+  } else {
+    platformPrice = 1; // No change
+  }
 
   // Calculate total price
-  let totalPrice = pagePrice + seoPrice + stylePrice + ecommercePrice + databasePrice + cmsPrice + platformPrice;
+  let totalPrice =
+    pagePrice +
+    seoPrice +
+    stylePrice +
+    ecommercePrice +
+    databasePrice +
+    cmsPrice +
+    platformPrice;
 
   // Construct the quote statement
-  let quoteStatement = `We estimate your project will cost ${totalPrice}. Please complete the form below to receive an email detailing the estimate.`;
-    
-  // Construct the quote details
-  let quoteDetails = `Page Count: ${pageCount}, SEO Page Count: ${seoPageCount}, Style Type: ${styleType}, E-commerce Type: ${ecommerceType}, CMS Type: ${cmsType}, Database Type: ${databaseType}, Platform Type: ${platformType}, Estimated Price: ${totalPrice}`;
+  let quoteStatement = `We estimate your project will cost $${totalPrice}. Please complete the form below to receive an email detailing the estimate.`;
 
+  // Construct the quote details
+  let quoteDetails = `Page Count: ${pageCount}, SEO Page Count: ${seoPageCount}, Style Type: ${styleType}, E-commerce Type: ${ecommerceType}, CMS Type: ${cmsType}, Database Type: ${databaseType}, Platform Type: ${platformType}, Estimated Price: $${totalPrice}`;
 
   // Set the quote statement and details in the emailForm page
   document.getElementById("quote-statement").innerText = quoteStatement;
   document.getElementById("quoteDetails").value = quoteDetails;
 
- // Switch between forms
+  // Switch between forms
   document.getElementById("mainPage").style.display = "none";
   document.getElementById("emailForm").style.display = "block";
-  
-
 }
